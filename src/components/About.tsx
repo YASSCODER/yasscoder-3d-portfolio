520fd2c532affb4904d36e98785f42b2e2f4c6be
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2023-07-14 10:56:20 +0100

    complete the About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,14 +14,13 @@ interface serviceCardProps {
 
 
 const ServiceCard: React.FC<serviceCardProps> = ({index, title, icon}) => {
-  const divOptions = {
-    max: 45,
-    scale: 1,
-    speed: 450,
-  };
   return(
-    <Tilt className="xs:w-[250px] w-full ">
+    <Tilt className="xs:w-[250px] w-full" options={{ max: 4, scale: 45, speed: 20 }}>
       <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div className='bg-tertiary rounded-[20px] py-5 px12 min-h-[280px] flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+          <h3 className='text-white font-bold text-[26px] text-center'>{title}</h3>
+        </div>
       </motion.div>
     </Tilt>
   )
